8806ea8a825a751a52cf54169d3f844ef961fff1
taha <mohammad.taha.jam@gmail.com>
2025-08-29 18:02:22 +0330

    push for props

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,29 @@
 import { ref } from 'vue'
 import ChildComponent from './pages/ChildComponent.vue'
 
-const message = ref('پیام اولیه از والد')
+const message = ref('frst mass')
+
+const secMessage = (newmass) => {
+  message.value = newmass
+}
+
+const showDialog = ref(false)
 </script>
 
 <template>
   <div style="padding: 20px">
-    <h1>والد</h1>
-    <p>پیام فعلی: {{ message }}</p>
+    <h4>parent</h4>
+    <p>{{ message }}</p>
 
-    <ChildComponent :msg="message" @msg="message = $event" />
+    <q-btn color="primary" label="باز کردن پاپ‌آپ" @click="showDialog = true" />
+
+    <q-dialog v-model="showDialog">
+      <q-card style="min-width: 400px; padding: 20px">
+        <ChildComponent :msg="message" @msg="secMessage" />
+        <div class="row justify-end q-gutter-sm">
+          <q-btn flat label="clse" color="negative" v-close-popup />
+        </div>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
--- src/pages/ChildComponent.vue
+++ src/pages/ChildComponent.vue
@@ -3,14 +3,14 @@ defineProps(['msg'])
 const emit = defineEmits(['msg'])
 
 function changeMessage() {
-  emit('msg', "پیام جدید از فرزند")
+  emit('msg', 'massi')
 }
 </script>
 
 <template>
-  <div style="margin-top:20px; border:1px solid gray; padding:10px">
-    <h2>فرزند</h2>
-    <p>پیام دریافتی از والد: {{ msg }}</p>
-    <button @click="changeMessage">تغییر پیام</button>
+  <div style="margin-top: 20px; border: 1px solid gray; padding: 10px">
+    <h2>childcomponent</h2>
+    <p>{{ msg }}</p>
+    <button @click="changeMessage"> changeMessage</button>
   </div>
 </template>
